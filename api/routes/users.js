e421d05d1db885a6f8556591fadff68d4e754785
KenWill24 <lwilliams@hylamobile.com>
2025-12-02 10:04:01 -0600

    Initial setup: client with Vite + api folder

---
+++ api/routes/users.js
@@ -0,0 +1,50 @@
+//load modules
+'use strict';
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models');
+const authenticateUser = require('../middleware/authenticateUser');
+
+/**
+ * GET /api/users
+ * Return the currently authenticated user
+ */
+router.get('/users', authenticateUser, async (req, res, next) => {
+  try {
+    const user = req.currentUser;
+
+    res.status(200).json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * POST /api/users
+ * Create a new user
+ */
+router.post('/users', async (req, res, next) => {
+  try {
+    // Create user
+    await User.create(req.body);
+    
+    // Set Location header
+    res.location('/');
+    res.status(201).end();
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.status(400).json({ message: 'Email address already exists.' });
+    } else if (error.name === "SequelizeValidationError") {
+      res.status(400).json({ message: error.errors.map(e => e.message) });
+    } else {
+      next(error);
+    }
+  }
+});
+
+module.exports = router;
